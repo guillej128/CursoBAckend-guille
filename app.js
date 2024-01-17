76fd0e1e993b3f930d2c4dd58f815f20a9ff85b8
guillej128 <139085820+guillej128@users.noreply.github.com>
2024-01-17 20:40:09 -0300

    primer entrega

--- app.js
+++ app.js
@@ -1,36 +1,23 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const productManager = require('./ProductManager'); 
-
+const express = require("express");
+// Creación de Servidor:
 const app = express();
-const PORT = 8080;
+const PUERTO = 8080;
 
-app.use(bodyParser.json());
+const productsRouter = require("./src/routes/products.router.js");
+const cartsRouter = require("./src/routes/carts.router.js");
 
-app.get('/products', (req, res) => {
-    const allProducts = productManager.getAllProducts();
-  
-    const limit = parseInt(req.query.limit, 10);
-    const limitedProducts = limit ? allProducts.slice(0, limit) : allProducts;
-  
-    res.json(limitedProducts);
-});
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/products/:id', (req, res) => {
-    const productId = parseInt(req.params.id, 10);
-    const product = productManager.getProductById(productId);
-  
-    if (!product) {
-        return res.status(404).json({ error: 'Producto no encontrado' });
-    } 
-  
-    res.json(product);
-});
+// Rutas
+app.use("/api/products", productsRouter);
+app.use("/api/carts/", cartsRouter);
 
 app.get('/', (req, res) => {
     res.send('¡Bienvenido a la página principal, proyecto Backend!');
 });
 
-app.listen(PORT, () => {
-    console.log(`Servidor en ejecución en http://localhost:${PORT}`);
+app.listen(PUERTO, () => {
+    console.log(`Funcionando en el Puerto ${PUERTO}`);
 });
